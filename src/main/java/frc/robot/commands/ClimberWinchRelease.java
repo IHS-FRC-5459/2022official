34da0f32129c6562840e1336385ab8ddd4c115f2
TobyFlowers <tobiasgflowers@gmail.com>
2022-03-17 16:21:40 -0400

    Fixes

---
+++ src/main/java/frc/robot/commands/ClimberWinchRelease.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class ClimberWinchRelease extends CommandBase {
+  /** Creates a new ClimberWinchRelease. */
+  public ClimberWinchRelease() {
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.getInstance().m_climberSub.winchFreeze();
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    RobotContainer.getInstance().m_climberSub.winchRelease();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    RobotContainer.getInstance().m_climberSub.winchFreeze();
+
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
